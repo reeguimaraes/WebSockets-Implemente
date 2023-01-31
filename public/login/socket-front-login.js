89e214da1be0cbd4321cd2d78e85a79e973fe287
Renato Guimarães <r_guiimaraes@hotmail.com>
2023-01-31 16:36:06 -0300

    versão 2 autenticando login

---
+++ public/login/socket-front-login.js
@@ -0,0 +1,12 @@
+const socket = io();
+
+function emitirAutenticarUsuario(dados) {
+    socket.emit("autenticar_usuario", dados);
+}
+socket.on("autenticacao_sucesso", () => {
+    alert("Usuario autenticado com sucesso!")
+    window.location.href = "/";
+});
+socket.on("autenticacao_erro", () => alert("Senha incorreta!"));
+socket.on("usuario_nao_encontrado", () => alert("Usuario não encontrado!"));
+export { emitirAutenticarUsuario };
---
+++ servidor/socket-back.js
@@ -0,0 +1,14 @@
+import registrarEventosDocumento from "./registrarEventos/documento.js";
+import registrarEventosInicio from "./registrarEventos/inicio.js";
+import registrarEventosCadastro from "./registrarEventos/cadastro.js";
+import registrarEventosLogin from "./registrarEventos/Login.js";
+
+import io from "./servidor.js";
+
+io.on("connection", (socket) => {
+  registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
+  registrarEventosInicio(socket, io);
+  registrarEventosDocumento(socket, io);
+;
+});
